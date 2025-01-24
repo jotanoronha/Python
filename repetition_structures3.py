532939e76d684db3286569ad9449e58dd941d0bf
jotanoronha <j.vitornoronha@gmail.com>
2025-01-23 22:49:41 -0300

    a better version of repetition_structures3

--- repetition_structures3.py
+++ repetition_structures3.py
@@ -1,13 +1,16 @@
 v = int(input("Digite um valor: "))
 
 # to  use the even version
+
 if v % 2 == 1:
     v = (v - 1)
 
     for i in range(v,-1,-2):
         print(i)
 
-else :
+#to use the odd version
+
+elif v % 2 == 0:
     for i in range(0,v,2):
         v = (v + 1)
         print(i)
