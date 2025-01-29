fb78e71bc4bafece7e4b943e4f805e3cd53afcda
jotanoronha <j.vitornoronha@gmail.com>
2025-01-29 17:19:04 -0300

    Some changes and improvements were made in this version and some conditional structures changed

--- repetition_structures3.py
+++ repetition_structures3.py
@@ -1,16 +1,70 @@
-v = int(input("Digite um valor: "))
+# This code counts and shows the amount of even and odd numbers between the values set by user.
 
-# to  use the even version
+v = int(input("Type a value: "))
+p = int(input("Type another value: "))
 
-if v % 2 == 1:
-    v = (v - 1)
+# Ask the user if the count will be even or odd
 
-    for i in range(v,-1,-2):
-        print(i)
+c = str(input("Witch number you want to see (odd or even)? "))
 
-#to use the odd version
 
-elif v % 2 == 0:
-    for i in range(0,v,2):
-        v = (v + 1)
-        print(i)
+
+if c == "odd":
+    if p > v:        
+        print(f"You choose, {c}")
+
+        if v % 2 != 1:
+            v = + 1
+
+            for i in range(v,p + 1,2):
+                print(i)
+        else:
+            for i in range(v,p + 1,2):
+                print(i)
+
+
+    else:
+        print(f"You choose, {c}")
+
+        if v % 2 != 1:
+            v = (v +1)
+
+            for x in range(p,v -1,2):
+                print(x)   
+        else:
+            for x in range(p,v -1,2):
+                print(x)
+
+        
+elif c == "even":
+
+    if p >= v:        
+        print(f"You choose, {c}")
+
+        if v % 2 != 0:
+            v, p = + 1 
+
+            for i in range(v,p + 1,2):     
+                print(i)
+        else:
+            for i in range(v,p + 1,2):     
+                print(i)
+
+
+# its cool use this expression below if you want to switch the start.
+# v > p
+# v, p = p, v
+
+    else:
+        print(f"You choose, {c}")
+        v >= p
+        if v % 2 != 0:
+            v = (v +1)
+
+            for x in range(p,v -1,2):
+                print(x)
+        else:
+            for x in range(p,v -1,2):
+                print(x) 
+else:    
+    print("You most type a value (odd or even).")
